7371bf8c1981953ac3ea9168c3992b860050de98
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2025-05-26 23:43:54 +0200

    green - sortSummary implementation

--- src/main/java/pl/kosieradzki/scoreboard/Summary.java
+++ src/main/java/pl/kosieradzki/scoreboard/Summary.java
@@ -7,7 +7,22 @@ public class Summary {
     private final List<Match> matches = new ArrayList<>();
 
     public void addMatch(Match match) {
-        matches.add(match);
+        if (matches.isEmpty()) {
+            matches.add(match);
+            return;
+        }
+        for (int i = 0; i < matches.size(); i++) {
+            if (match.getSum() > matches.get(i).getSum()) {
+                matches.add(i, match);
+                return;
+            } else if (match.getSum() == matches.get(i).getSum()) {
+                matches.add(i, match);
+                return;
+            }
+        }
+        if (match.getSum() < matches.get(matches.size() - 1).getSum()) {
+            matches.add(match);
+        }
     }
 
     public List<Match> getSummary() {
