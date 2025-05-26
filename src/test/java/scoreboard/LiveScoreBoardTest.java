6709e147d0f52047590f7e79a4c779c3da3eac27
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2025-05-26 23:20:26 +0200

    red - sortSummaryTest

--- src/test/java/scoreboard/LiveScoreBoardTest.java
+++ src/test/java/scoreboard/LiveScoreBoardTest.java
@@ -66,4 +66,26 @@ public class LiveScoreBoardTest {
 
         assertEquals(3, summary.getSummary().size());
     }
+
+    @Test
+    void sortSummaryTest(){
+        Summary summary = new Summary();
+
+        liveScoreBoard.startGame(new Match("Mexico", "Canada"));
+        summary.addMatch(liveScoreBoard.finishGame());
+
+        liveScoreBoard = new LiveScoreBoard();
+        liveScoreBoard.startGame(new Match("Spain", "Brazil"));
+        liveScoreBoard.updateScore( 10, 2);
+        summary.addMatch(liveScoreBoard.finishGame());
+
+        liveScoreBoard = new LiveScoreBoard();
+        liveScoreBoard.startGame(new Match("Germany", "France"));
+        liveScoreBoard.updateScore( 6, 6);
+        summary.addMatch(liveScoreBoard.finishGame());
+
+        assertEquals("Mexico", summary.getSummary().get(2).getHomeTeam());
+        assertEquals("Spain", summary.getSummary().get(1).getHomeTeam());
+        assertEquals("Germany", summary.getSummary().get(0).getHomeTeam());
+    }
 }
